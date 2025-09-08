b6f9100a4df1506c82ef3d7c8b250ffd91e8f278
Vitor França <vitor.sousa.franca@gmail.com>
2025-09-08 16:02:02 -0300

    fix: N=6 is the minimum to have a correct prediction

--- sin_exemple.py
+++ sin_exemple.py
@@ -45,7 +45,7 @@ input_range = [-1.5, 1.5]
 output_range = input_range
 
 # create the fuzzy model
-model = FuzzyTSModel(input_names=["var1", "var2", "var3"], output_name= "out", N=5,
+model = FuzzyTSModel(input_names=["var1", "var2", "var3"], output_name= "out", N=6,
                           input_range=input_range, output_range=output_range)
 
 # visualizing the created variables 
